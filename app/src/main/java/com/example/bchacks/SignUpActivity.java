413fb03c23379716d5a23fd34dc90f3ca390fb06
Rishali123 <rishali.vuriti@gmail.com>
2024-06-08 14:06:44 -0700

    sdf

--- app/src/main/java/com/example/bchacks/SignUpActivity.java
+++ app/src/main/java/com/example/bchacks/SignUpActivity.java
@@ -1,62 +1,103 @@
 package com.example.bchacks;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-public class SignUpActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class SignUpActivity extends AppCompatActivity
+{
+    EditText signupName, signupEmail, signupUsername, signupPassword, signupPhone, signupInterests;
+    TextView LoginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
     private FirebaseAuth auth;
-    private EditText signupEmail, signupPassword;
-    private Button signupButton;
-    private TextView loginRedirectText;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
         auth = FirebaseAuth.getInstance();
+
+        signupName = findViewById(R.id.signup_name);
         signupEmail = findViewById(R.id.signup_email);
+        signupPhone = findViewById(R.id.signup_phone);
+        signupInterests = findViewById(R.id.signup_interests);
+        signupUsername = findViewById(R.id.signup_username);
         signupPassword = findViewById(R.id.signup_password);
         signupButton = findViewById(R.id.signup_button);
-        loginRedirectText = findViewById(R.id.loginRedirectText);
-        signupButton.setOnClickListener(new View.OnClickListener() {
+        LoginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                String user = signupEmail.getText().toString().trim();
-                String pass = signupPassword.getText().toString().trim();
-                if (user.isEmpty()){
+            public void onClick(View v) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String name = signupName.getText().toString();
+                String email = signupEmail.getText().toString();
+                String phone = signupPhone.getText().toString();
+                String interests = signupInterests.getText().toString();
+                String username = signupUsername.getText().toString();
+                String password = signupPassword.getText().toString();
+
+                if (username.isEmpty()) {
                     signupEmail.setError("Email cannot be empty");
                 }
-                if (pass.isEmpty()){
+                if (password.isEmpty()) {
                     signupPassword.setError("Password cannot be empty");
-                } else{
-                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                } else {
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
+                                Log.i("Singup", "Signup Successful for : " + email + " - UID - " + auth.getCurrentUser().getUid() );
                                 Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                                String UID = auth.getCurrentUser().getUid();
+                                Log.i("Singup", "Signup - updating realtime database " + email + " - UID - " + UID );
+                                HelperClass helperClass = new HelperClass(name, email, phone, interests, username, password, UID);
+                                reference.child(username).setValue(helperClass);
                                 startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
                             } else {
+
                                 Toast.makeText(SignUpActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
+
+
                 }
             }
         });
-        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+
+        LoginRedirectText.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
+
     }
 }
