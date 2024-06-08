df810cdef8da7fb682f4c6c2a44d3fee9e90e204
Aishu85 <aishu.aravind5@gmail.com>
2024-06-08 14:51:25 -0700

    Swipe pt1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,10 +35,11 @@ android {
     }
 }
 
+
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("androidx.navigation:navigation-fragment:2.7.7")
@@ -48,4 +49,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.github.Yalantis:Koloda-Android:v0.0.2-alpha")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io" )}
+        jcenter()
+
     }
+
+
 }
 
 rootProject.name = "BCHacks"
