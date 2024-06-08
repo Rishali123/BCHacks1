7abf82494d63d800f077c3641dc2a6ecbb5b34c4
Rishali123 <rishali.vuriti@gmail.com>
2024-06-08 15:31:39 -0700

    working ver 1

--- app/src/main/java/com/example/bchacks/SwipeActivity.java
+++ app/src/main/java/com/example/bchacks/SwipeActivity.java
@@ -3,6 +3,7 @@ package com.example.bchacks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.yalantis.library.Koloda;
 
@@ -19,9 +20,15 @@ public class SwipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
 
+        Log.i("Swipe", "Check 1");
+
         koloda = findViewById(R.id.koloda);
+        Log.i("Swipe", "Check 2");
         list = new ArrayList<>();
+        Log.i("Swipe", "Check 3");
         adapter = new SwipeAdapter(this, list);
+        Log.i("Swipe", "Check 4");
         koloda.setAdapter(adapter);
+        Log.i("Swipe", "Check 5");
     }
 }
--- app/src/main/java/com/example/bchacks/SwipeAdapter.java
+++ app/src/main/java/com/example/bchacks/SwipeAdapter.java
@@ -1,6 +1,7 @@
 package com.example.bchacks;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +14,18 @@ public class SwipeAdapter extends BaseAdapter {
     private List<Integer> List;
 
     public SwipeAdapter(Context context, java.util.List<Integer> list) {
+        Log.i("Swipe", "Verify 1");
         this.context = context;
+        Log.i("Swipe", "Verify 2");
         List = list;
+        Log.i("Swipe", "Verify 3");
     }
 
     @Override
     public int getCount() {
         return 20;
     }
+
 
     @Override
     public Object getItem(int position) {
@@ -32,15 +37,19 @@ public class SwipeAdapter extends BaseAdapter {
         return 0;
     }
 
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view;
-
+        Log.i("Swipe", "Verify 7");
         if (convertView == null) {
+            Log.i("Swipe", "Verify 8");
             view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_koloda, parent, false);
         } else {
             view = convertView;
+            Log.i("Swipe", "Verify 9");
         }
-        return null;
+        Log.i("Swipe", "Verify 10");
+        return view;
     }
 }
