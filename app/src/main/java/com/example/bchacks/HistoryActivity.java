4aed93ac3c42a4045cd407f323e246b0881ae3b9
Aishu85 <aishu.aravind5@gmail.com>
2024-06-08 21:36:39 -0700

    Businesses

--- app/src/main/java/com/example/bchacks/HistoryActivity.java
+++ app/src/main/java/com/example/bchacks/HistoryActivity.java
@@ -26,14 +26,14 @@ public class HistoryActivity extends AppCompatActivity {
         binding = ActivityHistoryBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        int[] imageList = {R.drawable.arberplantcare, R.drawable.flauselectricflosser, R.drawable.glovewrap, R.drawable.legovaccum, R.drawable.nutrnutmilkmachine, R.drawable.provenskincare};
-        int[] ingredientList = {R.string.pastaIngredients, R.string.maggiIngredients,R.string.cakeIngredients,R.string.pancakeIngredients,R.string.pizzaIngredients, R.string.burgerIngredients, R.string.friesIngredients};
-        int[] descList = {R.string.pastaDesc, R.string.maggieDesc, R.string.cakeDesc,R.string.pancakeDesc,R.string.pizzaDesc, R.string.burgerDesc, R.string.friesDesc};
-        String[] nameList = {"Pasta", "Maggi", "Cake", "Pancake", "Pizza","Burgers", "Fries"};
-        String[] timeList = {"30 mins", "2 mins", "45 mins","10 mins", "60 mins", "45 mins", "30 mins"};
+        int[] imageList = {R.drawable.arberplantcare, R.drawable.flauselectricflosser, R.drawable.provenskincare};
+        //int[] ingredientList = {R.string.pastaIngredients, R.string.maggiIngredients,R.string.cakeIngredients,R.string.pancakeIngredients,R.string.pizzaIngredients, R.string.burgerIngredients, R.string.friesIngredients};
+        int[] descList = { R.string.ArberDesc, R.string.FlossDesc, R.string.ProvenDesc};
+        String[] nameList = {"Arber Plant Care", "Lily's Dental Hygiene", "Proven Cosmetics"};
+        //String[] timeList = {"30 mins", "2 mins", "45 mins","10 mins", "60 mins", "45 mins", "30 mins"};
 
         for (int i = 0; i < imageList.length; i++){
-            listData = new ListData(nameList[i], timeList[i], ingredientList[i], descList[i], imageList[i]);
+            listData = new ListData(nameList[i], descList[i], imageList[i]);
             dataArrayList.add(listData);
         }
         listAdapter = new ListAdapter(HistoryActivity.this, dataArrayList);
@@ -45,8 +45,8 @@ public class HistoryActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(HistoryActivity.this, SmallBusiness.class);
                 intent.putExtra("name", nameList[i]);
-                intent.putExtra("time", timeList[i]);
-                intent.putExtra("ingredients", ingredientList[i]);
+                //intent.putExtra("time", timeList[i]);
+                //intent.putExtra("ingredients", ingredientList[i]);
                 intent.putExtra("desc", descList[i]);
                 intent.putExtra("image", imageList[i]);
                 Log.i("History", "Going to intent");
--- app/src/main/java/com/example/bchacks/ListData.java
+++ app/src/main/java/com/example/bchacks/ListData.java
@@ -5,10 +5,10 @@ public class ListData {
     int ingredients, desc;
     int image;
 
-    public ListData(String name, String time, int ingredients, int desc, int image) {
+    public ListData(String name, int desc, int image) {
         this.name = name;
-        this.time = time;
-        this.ingredients = ingredients;
+        //this.time = time;
+        //this.ingredients = ingredients;
         this.desc = desc;
         this.image = image;
     }
